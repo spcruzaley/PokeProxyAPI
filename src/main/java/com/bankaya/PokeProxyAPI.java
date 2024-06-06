33ad96ae8c8db14d862e67c3dd96f0b28326d82a
SPCruzaley <sp_cruzaley@hotmail.com>
2024-06-06 17:31:08 -0600

    Ajuste de la clase de entrada

---
+++ src/main/java/com/bankaya/PokeProxyAPI.java
@@ -0,0 +1,12 @@
+package com.bankaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeProxyAPI {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeProxyAPI.class, args);
+    }
+}
